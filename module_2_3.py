752f51f30abb10f3e14bfc2b9af4d6ec700d759c
Bonick32 <Bonick3244@gmail.com>
2024-11-24 22:40:16 +0300

    2_3

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+
+while True:
+    num = my_list[0]
+    quantity = len(my_list)
+
+    if num >= 0 and quantity > 0:
+        print(num)
+        del my_list[0]
+        continue
+    elif num < 0 or quantity == 0:
+        break
